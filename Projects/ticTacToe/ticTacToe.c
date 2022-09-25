cfbcbab250a16219335b20e341f02b24b5228056
Johan Ravn <jran22@student.aau.dk>
2022-09-25 20:07:25 +0200

    game done

--- Projects/ticTacToe/ticTacToe.c
+++ Projects/ticTacToe/ticTacToe.c
@@ -25,7 +25,7 @@ int legalMove(char *board, int *move)
     }
 }
 
-char whichPlayer(char currentPlayer)
+char whichPlayer(int currentPlayer)
 {
     if (currentPlayer == 1)
     {
@@ -53,32 +53,66 @@ int checkGameOver(char *board)
     }
     if (freeSpaces <= 0)
     {
-        return 0;
+        return 1;
     }
 
-    // Checks verticals
+    // horizontal check
     for (int i = 0; i < 3; ++i)
     {
-        if (board[i * 3] == board[i * 3 + 1] == board[i * 3 + 2] && board[i * 3] != '_')
+        if (board[i * 3] == board[i * 3 + 1] && board[i * 3 + 1] == board[i * 3 + 2] && board[i * 3] != '_')
+        
         {
-            return 0;
+            return 1;
         }
     }
-    // Checks horizontals
+    
+    // vertical check
     for (int i = 0; i < 3; ++i)
     {
-        if (board[i * 3] == board[(1 + i) * 3 + ])
+        if (board[i] == board[i + 3] && board[i + 3] == board[i + 6] && board[i] != '_')
         {
-            return 0;
+            return 1;
         }
     }
+
+    // diagonal check
+    if (board[0] == board[4] && board[4] == board[8] && board[4] != '_')
+    {
+        return 1;
+    }
+
+    else if (board[2] == board[4] && board[4] == board[6] && board[4] != '_')
+    {
+        return 1;
+    }
+
+    return 0;
+}
+
+int* getMove()
+{
+    int move[2] = {0};
+
+    printf("\nChoose row: \n");
+    scanf("%d", &move[0]);
+    printf("\nChose column: \n");
+    scanf("%d", &move[1]);
+
+    return move;
+}
+
+void makeMove(char* board, int* move, int currenPlayer)
+{
+    board[move[0] * 3 + move[1]] = whichPlayer(currenPlayer);
 }
 
 int main()
 {
     printf("\n");
     // init board
-    char board[3][3] = {{'_', '_', '_'}, {'_', '_', '_'}, {'_', '_', '_'}};
+    char board[3][3] = {{'_', '_', '_'},
+                        {'_', '_', '_'},
+                        {'_', '_', '_'}};
     int currentPlayer = 1; // 1 == x, -1 == O;
     int gameStillGoing = 1;
 
@@ -87,43 +121,45 @@ int main()
 
     while (gameStillGoing == 1)
     {
-        int moveNotFound = 1;
-        int move[2] = {0};
+        // init
+        int moveFound = 0;
 
+        // drawboard
         drawBoard(board);
+        // debug: printf("\ncheckGameOver return: %d\n", checkGameOver((char *)board));
 
-        // Checks if game is over
-        if (checkGameOver((char *)board) == 0)
+        // Checks if winner
+        if (checkGameOver((char *) board) == 1)
         {
             gameStillGoing = 0;
             break;
         }
-
-        while (moveNotFound == 1)
+        // check for tie
+        else if (checkGameOver((char *) board) == 2)
         {
-            for (int i = 0; i < 3; ++i)
-            {
-                if (moveNotFound == 0)
-                {
-                    break;
-                }
-                for (int j = 0; j < 3; j++)
-                {
-                    int tryMove[2] = {i, j};
-                    if (legalMove((char *)board, tryMove) == 1)
-                    {
-                        move[0] = i;
-                        move[1] = j;
-                        moveNotFound = 0;
-                        break;
-                    }
-                }
-            }
+            gameStillGoing = 2;
+            break;
         }
-        board[move[0]][move[1]] = whichPlayer(currentPlayer);
-        currentPlayer = currentPlayer * -1;
+
+        // make move
+        while (moveFound == 0)
+        {
+            int tempPlaceHolder[2] = {-1};
+            int *move = getMove();
+            tempPlaceHolder[0] = *move;
+            tempPlaceHolder[1] = *(move + 1);
+
+            if (legalMove((char *) board, tempPlaceHolder) == 1)
+            {
+                makeMove((char*) board, tempPlaceHolder, currentPlayer);
+                currentPlayer = currentPlayer * -1;
+                moveFound = 1;
+            }
+
+        }
     }
-    printf("Game is over");
+    printf("\nGame is over\n");
+    printf("\n Player %c won", whichPlayer(currentPlayer * -1));
 
     return 0;
 }
