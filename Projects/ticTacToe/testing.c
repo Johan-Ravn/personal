201c203ca389b34f2527e2b18b3c7dd153d4ffc4
Johan Ravn <jran22@student.aau.dk>
2022-10-03 01:49:12 +0200

    rf

--- Projects/ticTacToe/testing.c
+++ Projects/ticTacToe/testing.c
@@ -1,16 +1,59 @@
 #include <stdio.h>
- 
-void print(int (*arr)[3])
+
+int gameState(char *board)
 {
-    int i, j;
-    for (i = 0; i < 3; i++)
-    for (j = 0; j < 3; j++)
-        printf("%d ", arr[i][j]);
+    // Check for tie
+    int freeSpace = 9;
+    for (int i = 0; i < 8; ++i)
+    {
+        if (board[i] != '_')
+        {
+            --freeSpace;
+        }
+    }
+
+    // Vertical and horizontal checks
+    for (int i = 0; i < 3; ++i)
+    {
+        if (board[i] == board[i + 3] && board[i + 3] == board[i + 6] && board[i] != '_')
+        {
+            return 1;
+        }
+
+        else if (board[0 + (i * 3)] == board[1 + (i * 3)] && board[1 + (i * 3)] == board[2 + (i * 3)] && board[0 + (i * 3)] != '_')
+        {
+            return 1;
+        }
+    }
+
+    // diagonal checks
+    if (board[0] == board[4] && board[4] == board[8] && board[4] != '_')
+    {
+        return 1;
+    }
+
+    else if (board[2] == board[4] && board[4] == board[6] && board[4] != '_')
+    {
+        return 1;
+    }
+
+    // tie
+    if (freeSpace <= 0)
+    {
+        return 2; // tie
+    }
+
+    // Game still going
+    return 0;
 }
- 
+
 int main()
-{   int M = 3;
-    int arr[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-    print(arr);
+{
+
+    char arr[9] = {'X', 'X', 'X',
+                  '_', '_', '_',
+                  '_', '_', '_'};
+    printf("Game state: %d\n", gameState(arr));
+
     return 0;
 }
